81e62d1a45c59382e8bab0ab23f08e94b168e51c
Sheraj Kc <88295555+kcsheraj@users.noreply.github.com>
2024-07-28 17:11:19 -0400

    add pre production changes.

--- api/index.js
+++ api/index.js
@@ -4,6 +4,7 @@ import dotenv from "dotenv";
 import userRoutes from "./routes/user.route.js";
 import authRoutes from "./routes/auth.route.js";
 import cookieParser from "cookie-parser";
+import path from "path";
 dotenv.config();
 
 mongoose
@@ -15,19 +16,24 @@ mongoose
     console.log(err);
   });
 
+const __dirname = path.resolve();
+
 const app = express();
 
-app.use(cookieParser());
+app.use(express.static(path.join(__dirname, "/client/dist")));
+
+app.get("*", (req, res) => {
+  res.sendFile(path.join(__dirname, "client", "dist", "index.html"));
+});
 
 app.use(express.json());
 
+app.use(cookieParser());
 app.listen(3000, () => {
-  console.log("Server is running on port 3000");
+  console.log("Server listening on port 3000");
 });
-
 app.use("/api/user", userRoutes);
 app.use("/api/auth", authRoutes);
-
 app.use((err, req, res, next) => {
   const statusCode = err.statusCode || 500;
   const message = err.message || "Internal Server Error";
