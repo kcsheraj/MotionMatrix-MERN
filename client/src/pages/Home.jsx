8d9390319b4f50bd4e2b63e2046851c79235d77a
Sheraj Kc <88295555+kcsheraj@users.noreply.github.com>
2024-07-28 17:53:49 -0400

    Update titles to MotionMatrix

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -4,7 +4,7 @@ export default function Home() {
   return (
     <div className="px-4 py-12 max-w-2xl mx-auto">
       <h1 className="text-3xl font-bold  mb-4 text-slate-800">
-        Welcome to my Auth App!
+        Welcome to MotionMatrix!
       </h1>
       <p className="mb-4 text-slate-700">
         This is a full-stack web application built with the MERN (MongoDB,
